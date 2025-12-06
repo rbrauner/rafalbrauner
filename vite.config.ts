225fb3abe95c01e937dfb8527cb5d59c0c095861
Rafał Brauner <rafalbrauner@gmail.com>
2025-12-06 19:08:51 +0100

    :construction: wip

--- vite.config.ts
+++ vite.config.ts
@@ -18,4 +18,9 @@ export default defineConfig({
             },
         }
     },
+    server: {
+        host: "0.0.0.0",
+        port: 5173,
+        origin: "http://rafalbrauner.localhost:5173",
+    },
 });
